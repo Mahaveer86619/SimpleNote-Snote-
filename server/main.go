12f4dff79b4abfcfcabea09e6a6447daf48fbed0
Mahaveer <psatyam86619@gmail.com>
2024-06-13 11:38:42 +0530

    Test commit

--- server/main.go
+++ server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	handleFunctions(mux)
+	
+	if err := http.ListenAndServe(":3030", mux); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func handleFunctions(mux *http.ServeMux) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `
+███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗
+██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝
+███████╗██║██╔████╔██║██████╔╝██║     █████╗  
+╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  
+███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗
+╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝
+                                              
+███╗   ██╗ ██████╗ ████████╗███████╗          
+████╗  ██║██╔═══██╗╚══██╔══╝██╔════╝          
+██╔██╗ ██║██║   ██║   ██║   █████╗            
+██║╚██╗██║██║   ██║   ██║   ██╔══╝            
+██║ ╚████║╚██████╔╝   ██║   ███████╗          
+╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝          
+                                          
+
+
+Serving at: http://127.0.0.1:3030
+
+Running in development mode
+		`)
+	})
 }
